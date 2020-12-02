d34b0ce9293c1a3c6499133f1a8abbccb447ada5
Iain Emsley <iain.emsley@warwick.ac.uk>
2020-12-02 12:27:27 +0000

    Update to app rating

--- google_play_scraper/scraper.py
+++ google_play_scraper/scraper.py
@@ -330,10 +330,7 @@ class PlayStoreScraper:
 		}
 
 		try:
-			if country in ["cn", "kr", "jp"]:
-				app["rating"] = rating[0][0][7][0][1]
-			else:
-				app["rating"] = rating[0][0][0][7][0][1]
+			app["rating"] = rating[0][0][7][0][1]
 		except TypeError:
 			app["rating"] = 0
 		except Exception:
